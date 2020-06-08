9d0361f83c65a5ab6ecdefb4741803ad0bf84d39
TomiXRM <chocolate0785lego@icloud.com>
2020-06-09 01:35:43 +0900

    first commit

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-var tokyoAlert = function() {
-	if (state){
-        window.alert("東京")
-	}
+var tokyoAlert = function () {
+        if (state) {
+                window.alert("東京")
+        }
 }
 
-var tokyoRelease = function(){
-	window.alert("解除")
+var tokyoRelease = function () {
+        window.alert("解除")
 }
 
-var trigger = function(){
-	state = true
-	document.bgColor = "red";
-	tokyoAlert()
+var trigger = function () {
+        state = true
+        document.bgColor = "red";
+        document.getElementById("body").setAttribute('style', "background-image:linear-gradient(-45deg,#ff0000 25 %,#000 25 %, #000 50 %,#ff0000 50 %, #ff0000 75 %,#000 75 %, #000);")
+        tokyoAlert()
 }
 
-var end = function(){
-	state = false
-	document.bgColor = "white";
-	tokyoRelease()
+var end = function () {
+        state = false
+        document.bgColor = "white";
+        tokyoRelease()
 }
 
-window.onload = function(){
-	setInterval(tokyoAlert,1500)
+window.onload = function () {
+        setInterval(tokyoAlert, 1500)
 }
